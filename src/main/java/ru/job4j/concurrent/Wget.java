982b24c04dea30e23af87f1062543300262dbee5
Elena Pakulina <el.pakulina@yandex.ru>
2024-04-16 16:07:26 +0500

    4. Режим ожидания. [#231217]

---
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -0,0 +1,23 @@
+package ru.job4j.concurrent;
+
+import java.sql.SQLOutput;
+
+public class Wget {
+    public static void main(String[] args) {
+        Thread thread = new Thread(
+                () -> {
+                    try {
+                        int index = 0;
+                        while (index <= 100) {
+                            System.out.print("\rLoading : " + index++ + "%");
+                            Thread.sleep(1000);
+                        }
+                        System.out.print("\nLoaded.");
+                    } catch (InterruptedException exception) {
+                        exception.printStackTrace();
+                    }
+                }
+        );
+        thread.start();
+    }
+}
