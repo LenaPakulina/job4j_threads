18363c5378e98df237779e153026cc0f39636031
Elena Pakulina <el.pakulina@yandex.ru>
2024-04-23 12:45:50 +0500

    1. Visibility. Общий ресурс вне критической секции [#1102 #492516]

--- src/main/java/ru/job4j/synchronize/io/ParseFile.java
+++ src/main/java/ru/job4j/synchronize/io/ParseFile.java
@@ -14,16 +14,24 @@ public class ParseFile {
         return file;
     }
 
-    public synchronized String getContent(Predicate<Character> filter) throws IOException {
+    private synchronized String parse(Predicate<Character> filter) throws IOException {
         StringBuilder result = new StringBuilder();
         try (var stream = new BufferedInputStream(new FileInputStream(file))) {
             int data;
-            while ((data = stream.read()) > 0) {
+            while ((data = stream.read()) != -1) {
                 if (filter.test((char) data)) {
                     result.append((char) data);
                 }
             }
         }
         return result.toString();
+    }
+
+    public String getContent() throws IOException {
+        return parse(character -> true);
+    }
+
+    public String getContentWithoutUnicode() throws IOException {
+        return parse(character -> (character < 0x80));
     }
 }
