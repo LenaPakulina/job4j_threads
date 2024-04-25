5aa9fa56ef509f97bdfe6cdc2b4292b01aa1fb40
Elena Pakulina <el.pakulina@yandex.ru>
2024-04-25 17:48:12 +0500

    2. Обеспечить остановку потребителя. [#66825 #492881]

--- src/main/java/ru/job4j/ParallelSearch.java
+++ src/main/java/ru/job4j/ParallelSearch.java
@@ -4,34 +4,34 @@ import java.util.function.Consumer;
 
 public class ParallelSearch {
 
-    public static void main(String[] args) {
-        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
-        Consumer<Thread> threadStop = Thread::interrupt;
+    public static void main(String[] args) throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>();
         final Thread consumer = new Thread(
                 () -> {
-                    while (true) {
+                    while (!Thread.currentThread().isInterrupted()) {
                         try {
                             System.out.println(queue.poll());
                         } catch (InterruptedException e) {
-                            e.printStackTrace();
                             Thread.currentThread().interrupt();
                         }
                     }
                 }
         );
         consumer.start();
-        Thread producer = new Thread(
-            () -> {
-                for (int index = 0; index != 3; index++) {
-                    queue.offer(index);
-                    try {
-                        Thread.sleep(500);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+        final Thread producer = new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        queue.offer(index);
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }
-                threadStop.accept(consumer);
-            }
         );
+        producer.start();
+        producer.join();
+        consumer.interrupt();
     }
 }
