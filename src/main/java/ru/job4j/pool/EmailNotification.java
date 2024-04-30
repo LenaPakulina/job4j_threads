9c7817ca67f2527d78734782944957c68caa7705
Elena Pakulina <el.pakulina@yandex.ru>
2024-04-30 22:22:30 +0500

    2. ExecutorService рассылка почты. [#63097 #493628]

--- src/main/java/ru/job4j/pool/EmailNotification.java
+++ src/main/java/ru/job4j/pool/EmailNotification.java
@@ -4,29 +4,28 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class EmailNotification {
-    private final ExecutorService pool;
-
-    public EmailNotification() {
-        this.pool = Executors.newFixedThreadPool(
-                Runtime.getRuntime().availableProcessors()
-        );
-    }
+    private final ExecutorService pool =
+            Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
     public void emailTo(User user) {
-        pool.submit(new Runnable() {
-            @Override
-            public void run() {
-                send(user.username(), null, user.email());
-            }
+         pool.submit(() -> {
+            send(user.username(), null, user.email());
         });
     }
 
     public void close() {
         pool.shutdown();
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public void send(String subject, String body, String email) {
-        System.out.printf("subject = Notification %s to email %s.\n", subject, email);
-        System.out.printf("body = Add a new event to %s.\n", subject);
+        System.out.printf("subject = Notification %s to email %s.%n", subject, email);
+        System.out.printf("body = Add a new event to %s.%n", subject);
     }
 }
